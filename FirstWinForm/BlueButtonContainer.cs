15efbff0b75cf4fe6c57fc1ad200171a4ac4da6c
Frank <frank@smartman.com.tw>
2024-08-30 16:30:58 +0800

    自訂控制項新增ButtonText屬性讓外部可以改

--- FirstWinForm/BlueButtonContainer.cs
+++ FirstWinForm/BlueButtonContainer.cs
@@ -12,7 +12,10 @@ namespace FirstWinForm
 {
     public partial class BlueButtonContainer : UserControl
     {
-        public Button InnerButton { get => this.BlueButtonControl;  }
+        // 按鈕控制項，只能讀取
+        public Button InnerButton { get => this.BlueButtonControl; }
+        // 按鈕的顯示文字，可以讀跟改按鈕的顯示文字
+        public string ButtonText { get => this.BlueButtonControl.Text; set => this.BlueButtonControl.Text = value; }
         public BlueButtonContainer()
         {
             InitializeComponent();
