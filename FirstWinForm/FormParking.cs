4187f74ccb328cf13754dc1e39dfe65d69b9d5ad
Frank <frank@smartman.com.tw>
2024-08-30 15:13:47 +0800

    新增註解

--- FirstWinForm/FormParking.cs
+++ FirstWinForm/FormParking.cs
@@ -26,6 +26,7 @@ namespace FirstWinForm
 
         private void InnerButton_Click(object? sender, EventArgs e)
         {
+            // const 代表這個變數是常數，不可變更
             const string url = "https://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=f4cc0b12-86ac-40f9-8745-885bddc18f79&rid=0daad6e6-0632-44f5-bd25-5e1de1e9146f";
             using (HttpClient http = new HttpClient())
             {
@@ -33,8 +34,8 @@ namespace FirstWinForm
                 // 使用Deserialize反序列化JSON資料，轉成物件
                 ParkingLotAPIResponse parkingLotAPIResponse = JsonSerializer.Deserialize<ParkingLotAPIResponse>(result) ?? new ParkingLotAPIResponse();
                 dataGridView1.DataSource = parkingLotAPIResponse.ParkingLots;
-              
-                List<string> headerNames = ["停車場編號","區域ID","區域名稱","停車場名稱","總停車格數量","狀態","剩餘空間","價格資訊","說明","位址","經度","緯度"];
+
+                List<string> headerNames = ["停車場編號", "區域ID", "區域名稱", "停車場名稱", "總停車格數量", "狀態", "剩餘空間", "價格資訊", "說明", "位址", "經度", "緯度"];
                 // set datagridview header names to above
                 for (int i = 0; i < headerNames.Count; i++)
                 {
@@ -72,7 +73,7 @@ namespace FirstWinForm
                         var worksheetByArea = workbook.Worksheets.Add(areaName);
                         // 用Where去篩選出區域名稱相同的停車場
                         var parkLotsByArea = parkingLotAPIResponse.ParkingLots.Where(p => p.AreaName == areaName).ToList();
-                        for (int i = 0;i < headerNames.Count;i++)
+                        for (int i = 0; i < headerNames.Count; i++)
                         {
                             worksheetByArea.Cell(1, i + 1).Value = headerNames[i];
                         }
