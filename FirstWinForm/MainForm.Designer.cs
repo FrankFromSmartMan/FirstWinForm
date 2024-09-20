bd77bcc2d8469ce3cb8b5ae9113e6dd81f021e99
Frank <frankhuang66655@gmail.com>
2024-09-20 21:49:07 +0800

    Add simple SQLite example

---
+++ FirstWinForm/MainForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace FirstWinForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonInsert = new Button();
+            dataGridViewScoreData = new DataGridView();
+            buttonCreateDB = new Button();
+            buttonTruncateDB = new Button();
+            buttonSelect = new Button();
+            textBoxName = new TextBox();
+            labelName = new Label();
+            label2 = new Label();
+            dateTimePickerBirthday = new DateTimePicker();
+            labelID = new Label();
+            buttonUpdate = new Button();
+            numericUpDownID = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewScoreData).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownID).BeginInit();
+            SuspendLayout();
+            // 
+            // buttonInsert
+            // 
+            buttonInsert.Location = new Point(162, 26);
+            buttonInsert.Name = "buttonInsert";
+            buttonInsert.Size = new Size(94, 29);
+            buttonInsert.TabIndex = 0;
+            buttonInsert.Text = "Insert Data";
+            buttonInsert.UseVisualStyleBackColor = true;
+            buttonInsert.Click += ButtonInsert_Click;
+            // 
+            // dataGridViewScoreData
+            // 
+            dataGridViewScoreData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewScoreData.Location = new Point(36, 105);
+            dataGridViewScoreData.Name = "dataGridViewScoreData";
+            dataGridViewScoreData.RowHeadersWidth = 51;
+            dataGridViewScoreData.Size = new Size(405, 311);
+            dataGridViewScoreData.TabIndex = 1;
+            // 
+            // buttonCreateDB
+            // 
+            buttonCreateDB.Location = new Point(36, 26);
+            buttonCreateDB.Name = "buttonCreateDB";
+            buttonCreateDB.Size = new Size(94, 29);
+            buttonCreateDB.TabIndex = 2;
+            buttonCreateDB.Text = "Create DB";
+            buttonCreateDB.UseVisualStyleBackColor = true;
+            buttonCreateDB.Click += ButtonCreateDB_Click;
+            // 
+            // buttonTruncateDB
+            // 
+            buttonTruncateDB.Location = new Point(36, 61);
+            buttonTruncateDB.Name = "buttonTruncateDB";
+            buttonTruncateDB.Size = new Size(94, 29);
+            buttonTruncateDB.TabIndex = 3;
+            buttonTruncateDB.Text = "Truncate DB";
+            buttonTruncateDB.UseVisualStyleBackColor = true;
+            buttonTruncateDB.Click += ButtonTruncateDB_Click;
+            // 
+            // buttonSelect
+            // 
+            buttonSelect.Location = new Point(162, 61);
+            buttonSelect.Name = "buttonSelect";
+            buttonSelect.Size = new Size(94, 29);
+            buttonSelect.TabIndex = 4;
+            buttonSelect.Text = "Select";
+            buttonSelect.UseVisualStyleBackColor = true;
+            buttonSelect.Click += ButtonSelect_Click;
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(540, 107);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(184, 27);
+            textBoxName.TabIndex = 5;
+            // 
+            // labelName
+            // 
+            labelName.AutoSize = true;
+            labelName.Location = new Point(471, 110);
+            labelName.Name = "labelName";
+            labelName.Size = new Size(49, 20);
+            labelName.TabIndex = 6;
+            labelName.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(470, 144);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Birthday";
+            // 
+            // dateTimePickerBirthday
+            // 
+            dateTimePickerBirthday.Format = DateTimePickerFormat.Custom;
+            dateTimePickerBirthday.Location = new Point(540, 144);
+            dateTimePickerBirthday.Name = "dateTimePickerBirthday";
+            dateTimePickerBirthday.Size = new Size(184, 27);
+            dateTimePickerBirthday.TabIndex = 8;
+            // 
+            // labelID
+            // 
+            labelID.AutoSize = true;
+            labelID.Location = new Point(471, 75);
+            labelID.Name = "labelID";
+            labelID.Size = new Size(24, 20);
+            labelID.TabIndex = 9;
+            labelID.Text = "ID";
+            // 
+            // buttonUpdate
+            // 
+            buttonUpdate.Location = new Point(540, 194);
+            buttonUpdate.Name = "buttonUpdate";
+            buttonUpdate.Size = new Size(94, 29);
+            buttonUpdate.TabIndex = 11;
+            buttonUpdate.Text = "Update";
+            buttonUpdate.UseVisualStyleBackColor = true;
+            buttonUpdate.Click += buttonUpdate_Click;
+            // 
+            // numericUpDownID
+            // 
+            numericUpDownID.Location = new Point(540, 73);
+            numericUpDownID.Name = "numericUpDownID";
+            numericUpDownID.ReadOnly = true;
+            numericUpDownID.Size = new Size(184, 27);
+            numericUpDownID.TabIndex = 12;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(numericUpDownID);
+            Controls.Add(buttonUpdate);
+            Controls.Add(labelID);
+            Controls.Add(dateTimePickerBirthday);
+            Controls.Add(label2);
+            Controls.Add(labelName);
+            Controls.Add(textBoxName);
+            Controls.Add(buttonSelect);
+            Controls.Add(buttonTruncateDB);
+            Controls.Add(buttonCreateDB);
+            Controls.Add(dataGridViewScoreData);
+            Controls.Add(buttonInsert);
+            Name = "MainForm";
+            Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewScoreData).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownID).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonInsert;
+        private DataGridView dataGridViewScoreData;
+        private Button buttonCreateDB;
+        private Button buttonTruncateDB;
+        private Button buttonSelect;
+        private TextBox textBoxName;
+        private Label labelName;
+        private Label label2;
+        private DateTimePicker dateTimePickerBirthday;
+        private Label labelID;
+        private Button buttonUpdate;
+        private NumericUpDown numericUpDownID;
+    }
+}
---
+++ FirstWinForm/MainForm.cs
@@ -0,0 +1,186 @@
+﻿
+using Dapper;
+
+namespace FirstWinForm
+{
+
+    public partial class MainForm : Form
+    {
+        // The name of the SQLite database file
+        readonly string sqliteFileName = "MyDatabase.db";
+        // The connection string of the SQLite database
+        readonly string sqliteConnString = "Data Source=MyDatabase.db";
+        public MainForm()
+        {
+            InitializeComponent();
+            CreateDocument();
+            InitControls();
+        }
+        // Initialize the controls
+        void InitControls()
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+            // Set the maximum and minimum values of the NumericUpDown control
+            numericUpDownID.Maximum = int.MaxValue;
+            // Set the format of the DateTimePicker control
+            dateTimePickerBirthday.Format = DateTimePickerFormat.Custom;
+            dateTimePickerBirthday.CustomFormat = "yyyy-MM-dd";
+            // Register the event of clicking the cell of the DataGridView
+            dataGridViewScoreData.CellMouseClick += DataGridViewScoreData_CellMouseClick;
+        }
+        private void DataGridViewScoreData_CellMouseClick(object? sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Set the value of the NumericUpDown control
+                numericUpDownID.Value = (int)dataGridViewScoreData.Rows[e.RowIndex].Cells[0].Value;
+                // fetch single user from sqlite
+                using (var conn = new System.Data.SQLite.SQLiteConnection(sqliteConnString))
+                {
+                    conn.Open();
+                    var user = conn.QueryFirstOrDefault<User>("SELECT * FROM Users WHERE Id = @Id", new { Id = numericUpDownID.Value });
+                    if (user != null)
+                    {
+                        textBoxName.Text = user.Name;
+                        dateTimePickerBirthday.Value = user.Birthday;
+                    }
+                    conn.Close();
+                }
+            }
+        }
+
+        void CreateDocument()
+        {
+            // Create a SQLite document if not exist
+            if (!File.Exists(sqliteFileName))
+            {
+                System.Data.SQLite.SQLiteConnection.CreateFile(sqliteFileName);
+                MessageBox.Show("Database created successfully!");
+            }
+        }
+        private void ButtonInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<User> list = new List<User>()
+                {
+                    new User(1, "John", new DateTime(1990, 1, 1)),
+                    new User(2, "Frank", new DateTime(1994, 3, 14)),
+                    new User(3, "Maomi", new DateTime(2021, 10, 10)),
+                };
+                // Create a connection to the database
+                using (var conn = new System.Data.SQLite.SQLiteConnection(sqliteConnString))
+                {
+                    conn.Open();
+                    // insert mock data to sqlite if the table has no data usinf dapper
+                    int count = conn.ExecuteScalar<int>("SELECT COUNT(*) FROM Users");
+                    if (count == 0)
+                    {
+                        conn.Execute("INSERT INTO Users (Name, Birthday) VALUES (@Name, @Birthday)", list);
+                    }
+
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "---" + ex.StackTrace);
+            }
+        }
+
+        private void ButtonCreateDB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var conn = new System.Data.SQLite.SQLiteConnection(sqliteConnString))
+                {
+                    conn.Open();
+                    // Create a table of users if it does not exist using dapper
+                    conn.Execute("CREATE TABLE IF NOT EXISTS Users (Id INTEGER PRIMARY KEY, Name TEXT, Birthday DATE)");
+                    conn.Close();
+                    MessageBox.Show("Table created successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "---" + ex.StackTrace);
+            }
+        }
+
+        private void ButtonTruncateDB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var conn = new System.Data.SQLite.SQLiteConnection(sqliteConnString))
+                {
+                    conn.Open();
+                    // Truncate the table users using dapper
+                    conn.Execute("DELETE FROM Users");
+                    conn.Close();
+                    MessageBox.Show("Table dropped successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "---" + ex.StackTrace);
+            }
+        }
+
+        private void ButtonSelect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var conn = new System.Data.SQLite.SQLiteConnection(sqliteConnString))
+                {
+                    conn.Open();
+                    // Select all users from the table using dapper
+                    List<User> users = conn.Query<User>("SELECT * FROM Users").ToList();
+                    conn.Close();
+                    dataGridViewScoreData.DataSource = users;
+                    // set birthday format to yyyy-MM-dd
+                    dataGridViewScoreData.Columns[2].DefaultCellStyle.Format = "yyyy-MM-dd";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "---" + ex.StackTrace);
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            var user = new User(id: (int)numericUpDownID.Value, name: textBoxName.Text, birthday: dateTimePickerBirthday.Value);
+            try
+            {
+                using (var conn = new System.Data.SQLite.SQLiteConnection(sqliteConnString))
+                {
+                    conn.Open();
+                    // Update the user using dapper
+                    conn.Execute("UPDATE Users SET Name = @Name, Birthday = @Birthday WHERE Id = @Id", user);
+                    conn.Close();
+                    MessageBox.Show("User updated successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "---" + ex.StackTrace);
+            }
+        }
+    }
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public DateTime Birthday { get; set; }
+        public User()
+        {
+
+        }
+        public User(int id, string name, DateTime birthday)
+        {
+            Id = id;
+            Name = name;
+            Birthday = birthday;
+        }
+    }
+}
